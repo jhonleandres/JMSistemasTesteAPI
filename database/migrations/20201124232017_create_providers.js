fea04a2c2992b9e0cfebd90c99c5fa79a4aeaa56
JHONLEANDRES <jhonleandres.silva@gmail.com>
2020-11-25 01:27:00 -0300

    fix: columns tables

--- database/migrations/20201124232017_create_providers.js
+++ database/migrations/20201124232017_create_providers.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
     return knex.schema.createTable('providers', (table) => {
-      table.increments();
+      table.increments('id').primary();
       table.string('name').notNullable().unique();
       table.timestamps(false, true);
     });
--- database/migrations/20201124232025_create_billsToPay.js
+++ database/migrations/20201124232025_create_billsToPay.js
@@ -1,16 +1,16 @@
 exports.up = function (knex) {
     return knex.schema.createTable('billsToPay', (table) => {
-        table.increments();
+        table.increments('id').primary();
         table.string('descrition').notNullable();
         table
             .integer('providerId')
+            .unsigned()
             .references('id')
             .inTable('providers');
         table.decimal('price').notNullable();
-        table.datetime('releaseDate').notNullable();
+        table.datetime('releaseDate').notNullable().defaultTo(knex.fn.now());
         table.datetime('dueDate').notNullable();
         table.enu('status', ['ABERTO','PAGO', 'VENCIDO']).notNullable();
-
         table.timestamps(false, true);
     });
   };
