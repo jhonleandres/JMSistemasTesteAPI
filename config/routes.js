9463fa48aeda3e9db4ffabc11af1ba24f8e71ad9
JHONLEANDRES <jhonleandres.silva@gmail.com>
2020-11-25 02:10:07 -0300

    add rotas

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,21 @@
 
 module.exports = app => {
+    
+    app.route('/provider')
+        .post(app.api.FornecedorController.save)
+        .get(app.api.FornecedorController.get)
+        .get(app.api.FornecedorController.getById)
+        
+    app.route('/provider/:id')
+        .get(app.api.FornecedorController.getById)
+
     app.route('/billstopay')
         .post(app.api.ContasPagarController.save)
         .get(app.api.ContasPagarController.get)
+        .get(app.api.ContasPagarController.getById)
+        .get(app.api.ContasPagarController.getFilterType)
+
+    app.route('/billstopay/:id')
         .get(app.api.ContasPagarController.getById)
         
 }
